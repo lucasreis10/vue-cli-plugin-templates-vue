cf4b7038f0cb5a70a2efc3d27d435eedd972fe1e
Lucas Reis <lucaschristian10@gmail.com>
2021-02-27 01:40:29 -0300

    feat: adicionar opcoes de tarefas no prompt

--- generator/index.js
+++ generator/index.js
@@ -3,78 +3,82 @@ const { EOL } = require('os')
 
 
 module.exports = function(api, options) {
-  const { nomeModulo, pathFileStore } = options;
 
-  const nomeModuloLowerCase = nomeModulo.toLowerCase();
-  const nomeModuloUpperCase = nomeModulo.toUpperCase();
+  const { nomeModulo, pathFileStore, tipoTarefa } = options;
+  
+  if(tipoTarefa === 'tarefaStore') {
+    
+    const nomeModuloLowerCase = nomeModulo.toLowerCase();
+    const nomeModuloUpperCase = nomeModulo.toUpperCase();
 
-  // Renderizar template store
-  api.render('./templates', { nomeModulo, nomeModuloLowerCase });
+    // Renderizar template store
+    api.render('./templates', { nomeModulo, nomeModuloLowerCase });
 
 
-  // Adicionar import de novo store em aquivo store.ts
-  api.injectImports(pathFileStore, `import ${nomeModuloLowerCase}Module, { ${nomeModuloUpperCase}_MODULE } from '@/store/modules/${nomeModuloLowerCase}/${nomeModuloLowerCase}Module';`);
+    // Adicionar import de novo store em aquivo store.ts
+    api.injectImports(pathFileStore, `import ${nomeModuloLowerCase}Module, { ${nomeModuloUpperCase}_MODULE } from '@/store/modules/${nomeModuloLowerCase}/${nomeModuloLowerCase}Module';`);
 
 
-  // Adicionar novo module store em arquivo store.ts
-  api.afterInvoke(() => {
-    const espacos = 4;
-    const arquivoStore = api.resolve(pathFileStore);
-    const conteudoArquivo = fs.readFileSync(arquivoStore, { encoding: 'utf-8' })
-    const lines = conteudoArquivo.split(/\r?\n/g)
+    // Adicionar novo module store em arquivo store.ts
+    api.afterInvoke(() => {
+      const espacos = 4;
+      const arquivoStore = api.resolve(pathFileStore);
+      const conteudoArquivo = fs.readFileSync(arquivoStore, { encoding: 'utf-8' })
+      const lines = conteudoArquivo.split(/\r?\n/g)
 
-    const modulesObj = lines.findIndex(line => line.match(/modules\:/))
+      const modulesObj = lines.findIndex(line => line.match(/modules\:/))
 
-    let found = false;
-    let index = modulesObj;
+      let found = false;
+      let index = modulesObj;
 
-    while(!found){
-      let linhaObjModules = lines[index+1];
-      if(linhaObjModules.indexOf('}') !== -1) {
-        var tabs = ((lines[index].match(new RegExp("\t", "g")) || []).length);
-        found = true;
+      while(!found){
+        let linhaObjModules = lines[index+1];
+        if(linhaObjModules.indexOf('}') !== -1) {
+          var tabs = ((lines[index].match(new RegExp("\t", "g")) || []).length);
+          found = true;
+        }
+        index++;
       }
-      index++;
-    }
 
-    const EspacosETabs = `${new Array(tabs).fill('\t').join('')}${new Array(espacos).fill(' ').join('')}`;
+      const EspacosETabs = `${new Array(tabs).fill('\t').join('')}${new Array(espacos).fill(' ').join('')}`;
 
-    lines.splice(index, 0, `${EspacosETabs}[${nomeModuloUpperCase}_MODULE]: ${nomeModuloLowerCase}Module,`);
-    fs.writeFileSync(api.resolve(arquivoStore), lines.join(EOL), { encoding: 'utf-8' })
-  })
-
-  api.onCreateComplete(() => {
-
-    // Renomear pasta para name module escolhido no prompt
-    fs.renameSync('src/store/modules/new-module', `src/store/modules/${nomeModuloLowerCase}`);
-
-    // Renomear arquivos para name module escolhido no prompt
-    const pathParaRenomearArquivos = `src/store/modules/${nomeModuloLowerCase}`;
-    const files = [
-      {
-        nomeArquivoAtual: `${pathParaRenomearArquivos}/newActions.ts`,
-        novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Actions.ts`
-      },
-      {
-        nomeArquivoAtual: `${pathParaRenomearArquivos}/newGetters.ts`,
-        novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Getters.ts`
-      },
-      {
-        nomeArquivoAtual: `${pathParaRenomearArquivos}/newModule.ts`,
-        novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Module.ts`
-      },
-      {
-        nomeArquivoAtual: `${pathParaRenomearArquivos}/newMutations.ts`,
-        novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Mutations.ts`
-      },
-    ];
-
-    files.forEach((file) => {
-      fs.rename(file.nomeArquivoAtual, file.novaNomeArquivo, function(err) {
-        if ( err ) console.log('ERROR: ' + err);
-      });
+      lines.splice(index, 0, `${EspacosETabs}[${nomeModuloUpperCase}_MODULE]: ${nomeModuloLowerCase}Module,`);
+      fs.writeFileSync(api.resolve(arquivoStore), lines.join(EOL), { encoding: 'utf-8' })
     })
-  })
+
+    api.onCreateComplete(() => {
+
+      // Renomear pasta para name module escolhido no prompt
+      fs.renameSync('src/store/modules/new-module', `src/store/modules/${nomeModuloLowerCase}`);
+
+      // Renomear arquivos para name module escolhido no prompt
+      const pathParaRenomearArquivos = `src/store/modules/${nomeModuloLowerCase}`;
+      const files = [
+        {
+          nomeArquivoAtual: `${pathParaRenomearArquivos}/newActions.ts`,
+          novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Actions.ts`
+        },
+        {
+          nomeArquivoAtual: `${pathParaRenomearArquivos}/newGetters.ts`,
+          novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Getters.ts`
+        },
+        {
+          nomeArquivoAtual: `${pathParaRenomearArquivos}/newModule.ts`,
+          novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Module.ts`
+        },
+        {
+          nomeArquivoAtual: `${pathParaRenomearArquivos}/newMutations.ts`,
+          novaNomeArquivo: `${pathParaRenomearArquivos}/${nomeModuloLowerCase}Mutations.ts`
+        },
+      ];
+
+      files.forEach((file) => {
+        fs.rename(file.nomeArquivoAtual, file.novaNomeArquivo, function(err) {
+          if ( err ) console.log('ERROR: ' + err);
+        });
+      })
+    })
+  }
 }
 
 
--- prompts.js
+++ prompts.js
@@ -1,6 +1,25 @@
-module.exports = pkg => {
-  const prompts = [
+module.exports = [
     {
+      name: 'tipoTarefa',
+      type: 'list',
+      message: 'Escolha o tipo de terefa',
+      default: '^11.0.0',
+      choices: [
+        {
+          name: 'Criar module store Vuex',
+          value: 'tarefaStore',
+          short: 'Module store'
+        },
+        {
+          name: 'Criar arquivos de teste',
+          value: 'tarefaTeste',
+          short: 'Arquivo de teste'
+        },
+      ],
+      default: 'moduleStore'
+    },
+    {
+      when: res => res.tipoTarefa === 'tarefaStore',
       name: 'nomeModulo',
       type: 'input',
       message: 'Escolha nome do modulo para sua store',
@@ -8,13 +27,11 @@ module.exports = pkg => {
       default: 'NAME_MODULE'
     },
     {
+      when: res => res.tipoTarefa === 'tarefaStore',
       name: 'pathFileStore',
       type: 'input',
       message: 'Digite o caminho para o arquivo store.ts ou deixe o padrao',
       validade: input => !!input,
       default: 'src/store/store.ts'
     },
-  ]
-
-  return prompts
-}
+]
